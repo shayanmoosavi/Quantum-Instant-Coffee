bdf6cf8f362c56cafd6163afed6c2f8d2478931c
Shayan Moosavi <moosavi.shayan@gmail.com>
2025-04-27 00:01:57 +0330

    added support for projected density of states (PDOS) calculations and their corresponding input/output file patterns

--- config.py
+++ config.py
@@ -84,6 +84,8 @@ class ProjectConfig:
                 "scf_soc": "spin_orbit/scf",  # Directory for spin-orbit SCF calculations
                 "projected_bands": "projected_bands",  # Directory for projected band data
                 "projected_bands_soc": "spin_orbit/projected_bands",  # Directory for spin-orbit projected bands
+                "pdos": "pdos",  # Directory for projected density of states (PDOS) calculations
+                "pdos_soc": "spin_orbit/pdos",  # Directory for spin-orbit PDOS calculations
                 "wannier": "wannier",  # Directory for Wannier calculations
                 "wannier_soc": "spin_orbit/wannier",  # Directory for spin-orbit Wannier calculations
                 "strain": "strain",  # Directory for strain-related data
@@ -92,22 +94,27 @@ class ProjectConfig:
             },
             file_patterns=FilePatterns(
                 input={
-                    "vc_relax_input": "{compound_name}_vc_relax{flag}.pw.in",
+                    "relax_input": "{compound_name}_relax{flag}.pw.in",  # Pattern for relax input files
+                    "vc_relax_input": "{compound_name}_vc_relax{flag}.pw.in", # Pattern for vc-relax input files
                     "scf_input": "{compound_name}_scf{flag}.pw.in",  # Pattern for SCF input files
                     "pw_bands_input": "{compound_name}_bands{flag}.pw.in",  # Pattern for PW Bands input files
                     "kpdos_input": "{compound_name}{flag}.kpdos.in",  # Pattern for KPDOS input files
                     "bands_input": "{compound_name}{flag}.bands.in",  # Pattern for Bands input files
+                    "nscf_input": "{compound_name}_nscf{flag}.pw.in",  # Pattern for NSCF input files
+                    "pdos_input": "{compound_name}{flag}.pdos.in",  # Pattern for PDOS input files
                     "nscf_wannier_input": "{compound_name}_nscf_wannier{flag}.pw.in", # Pattern for NSCF wannier input files
                     "pw2wan_input": "{compound_name}{flag}.pw2wan.in",  # Pattern for pw2wannier90 input files
                     "wannier_input": "{compound_name}_wannier{flag}.win",  # Pattern for wannier input files
                 },
                 output={
+                    "relax_output": "{compound_name}_relax{flag}.pw.out",  # Pattern for relax output files
                     "vc_relax_output": "{compound_name}_vc_relax{flag}.pw.out",  # Pattern for vc-relax output files
                     "scf_output": "{compound_name}_scf{flag}.pw.out",  # Pattern for SCF output files
                     "pw_bands_output": "{compound_name}_bands{flag}.pw.out",  # Pattern for PW Bands output files
                     "kpdos_output": "{compound_name}{flag}.kpdos.out",  # Pattern for KPDOS output files
                     "projbands_output": "{compound_name}{flag}.projbands", # Pattern for generated projbands files from the AWK script
                     "bands_gnu": "{compound_name}.bands.gnu", # Pattern for calculated band data
+                    "nscf_output": "{compound_name}_nscf{flag}.pw.out",  # Pattern for NSCF output files
                 }
             )
         )
