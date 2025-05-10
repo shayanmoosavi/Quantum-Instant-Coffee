1e1550f4cd2f3b7fdc2ebf93763d62a46712c8a6
Shayan Moosavi <moosavi.shayan@gmail.com>
2025-05-11 02:07:16 +0330

    updated the compare bands script to reflect the new changes

--- compare_bands_plotter.py
+++ compare_bands_plotter.py
@@ -2,8 +2,12 @@
 
 This script initializes a project, processes Wannier and DFT comparison data, and generates comparison plots.
 """
-from utils.plot_handler import *
-from data.data_processor import *
+from sys import argv
+
+from core.project_setup import initialize_project
+from data.data_processor import process_comparison_data
+from data.dft_info_extractor import prepare_wannier_info
+from utils.plot_handler import BandsPlotConfig, plot_wannier_comparison
 
 # Initializing the project
 project = initialize_project(argv, is_input=False, is_wannier=True)
@@ -13,6 +17,11 @@ prepare_wannier_info(project)
 
 # Processing the comparison data for Wannier and DFT band structures.
 process_comparison_data(project)
+
+# Setting the plot configuration
+plot_config = BandsPlotConfig()
+# You can modify the plot configuration here
+# ...
 
 # Plotting the comparison of DFT and Wannier band structures.
 plot_wannier_comparison(project, save_fig=False)
