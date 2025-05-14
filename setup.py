b7001208d0aab3766e854e522f1033f0bf0a33e1
Shayan Moosavi <moosavi.shayan@gmail.com>
2025-05-15 01:04:12 +0330

    added base and fetch_info requirements files; updated setup.py for new dependencies and version

--- setup.py
+++ setup.py
@@ -3,11 +3,28 @@ from setuptools import setup, find_packages
 
 setup(
     name="quantum_instant_coffee",
-    version="1.0",
+    version="1.1",
     packages=find_packages(),
     install_requires=[
         "numpy",
         "matplotlib",
         "pandas",
+        "rich"
     ],
+    extras_requires={
+        "fetch_info": [
+            "requests",
+            "lxml",
+            "beautifulsoup4"
+        ],
+        "all": [
+            "numpy",
+            "matplotlib",
+            "pandas",
+            "rich",
+            "requests",
+            "beautifulsoup4",
+            "lxml"
+        ]
+    }
 )
