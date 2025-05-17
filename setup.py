33632fa4b34b968d4cbc78c86a12d3dc141cd1a7
Shayan Moosavi <moosavi.shayan@gmail.com>
2025-05-17 17:28:10 +0330

    updated to version 1.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="quantum_instant_coffee",
-    version="1.2",
+    version="1.3",
     packages=find_packages(),
     install_requires=[
         "numpy",
